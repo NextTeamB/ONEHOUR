92c23b5ff69b70a5a9d435dd428306d959abacdd
Dropper-in <dropperin59@gmail.com>
2023-07-24 14:34:26 +0900

    #10 feat: 페이지 이동 구현 및 아이콘 삽입, 코드 리팩토링 Link로 페이지 이동, state 값 변경을 통해 Image 리렌더, 맵 함수 리팩토링

--- components/navigator/Navigator.tsx
+++ components/navigator/Navigator.tsx
@@ -1,8 +1,9 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
-import styles from './Navigator.module.css';
+import styles from './Navigator.module.scss';
 import Image from 'next/image';
 import logo from '../../public/logo.png';
 import Icon1 from '../../public/homeIcon.png';
@@ -14,53 +15,106 @@ import challengeIcon from '../../public/fireIcon_white.png';
 import profileIcon from '../../public/userIcon_login.png';
 
 const Navigator = () => {
-	const menus: string[] = [
-		'홈화면',
-		'원아워 레코즈',
-		'마이페이지',
-		'챌린저스',
-		'설정',
+	var displaynone: number = 990;
+	var tablet: number = 1200;
+	var notebook: number = 1440;
+	interface menuProvider {
+		name: string;
+		path: string;
+		icon: any;
+	}
+	interface iconSizeProvider {
+		menuIconSize: number;
+		profileIconSize: number;
+		challengeIconSize: number;
+	}
+	const [iconSize, setIconSize] = useState<iconSizeProvider>({
+		menuIconSize: 30,
+		profileIconSize: 25,
+		challengeIconSize: 26,
+	});
+	const menuData: menuProvider[] = [
+		{ name: '홈화면', path: '/home', icon: Icon1 },
+		{ name: '원아워 레코즈', path: '/records', icon: Icon2 },
+		{ name: '마이페이지', path: '/mypage', icon: Icon3 },
+		{ name: '챌린저스', path: '/challengers', icon: Icon4 },
+		{ name: '설정', path: '/settings', icon: Icon5 },
 	];
-	const images: any[] = [Icon1, Icon2, Icon3, Icon4, Icon5];
 	const subMenus: string[] = ['개인정보 처리방침', '이용 약관', '로그아웃'];
+
+	useEffect(() => { //window 너비 변경을 감지하여 아이콘 사이즈 변경
+		const onWindowResize = (e: Event) => {
+			var width: number = window.innerWidth;
+			var newIconSize = { ...iconSize };
+			if (displaynone < width && width <= tablet) {
+				newIconSize.menuIconSize = 20;
+				newIconSize.profileIconSize = 18;
+				newIconSize.challengeIconSize = 18;
+			} else if (tablet < width && width <= notebook) {
+				newIconSize.menuIconSize = 25;
+				newIconSize.profileIconSize = 20;
+				newIconSize.challengeIconSize = 22;
+			} else {
+				newIconSize.menuIconSize = 30;
+				newIconSize.profileIconSize = 25;
+				newIconSize.challengeIconSize = 26;
+			}
+			setIconSize(newIconSize);
+		};
+
+		window.addEventListener('resize', onWindowResize);
+
+		return () => {
+			window.removeEventListener('resize', onWindowResize);
+		};
+	}, []);
 	return (
 		<div className={styles.navWrapper}>
 			<div className={styles.logoWrapper}>
 				<Image src={logo} width={108} alt='logoImage'></Image>
-				원아워
+				<span>원아워</span>
 			</div>
 			<hr className={styles.breakline}></hr>
 			<div className={styles.profileWrapper}>
-				<div className={styles.profile}>
-					<Image src={profileIcon} alt='기본 프로필 이미지' width={25} />
-				</div>
-				<p>
-					<span className={styles.bold}>홍당무</span> 님
-				</p>
+				<Link href='/home'>
+					<div className={styles.profile}>
+						<Image
+							src={profileIcon}
+							alt='기본 프로필 이미지'
+							width={iconSize.profileIconSize}
+						/>
+					</div>
+					<p>
+						<span>홍당무</span> 님
+					</p>
+				</Link>
 			</div>
 			<hr className={styles.breakline}></hr>
-			<div
-				style={{
-					marginTop: '10px',
-					height: '90px',
-					display: 'flex',
-					justifyContent: 'center',
-					alignItems: 'center',
-					fontWeight: '800',
-				}}>
+			<div className={styles.commentWrapper}>
 				<div>착실하게 하루 한 시간! 원아워와 함께 해요</div>
 			</div>
 			<div className={styles.challengeButton}>
-				<Image src={challengeIcon} alt='challengeIcon' width={26}></Image>
+				<Image
+					src={challengeIcon}
+					alt='challengeIcon'
+					width={iconSize.challengeIconSize}></Image>
 				<div style={{ marginLeft: '10px' }}>CHALLENGE</div>
 			</div>
 			<div className={styles.menuWrapper}>
-				{menus.map((menu, index) => (
-					<div className={styles.menuList} key={index}>
-						<Image src={images[index]} alt='menuIcon' width={30} />
-						<div className={styles.menuText}>{menu}</div>
-					</div>
-				))}
+				{menuData.map((menu, index) => {
+					return (
+						<div key={index}>
+							<Link href={menu.path} className={styles.menuList}>
+								<Image
+									src={menu.icon}
+									alt='menuIcon'
+									width={iconSize.menuIconSize}
+								/>
+								<div className={styles.menuText}>{menu.name}</div>
+							</Link>
+						</div>
+					);
+				})}
 			</div>
 			<div className={styles.subMenuWrapper}>
 				{subMenus.map((menu, index) => (
