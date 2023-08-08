9d29643f05e52a52f8996f905580096edfb24ca5
민종 <s2hoon326@gmail.com>
2023-08-08 16:41:10 +0900

    #25 feat: Axios 도입 및 더미데이터 테스트 Axios 적용하여 로컬 데이터 파일 GET 요청 및 POST 요청 테스팅 GET / POST 무한요청 오류사항 해결 완료 추가사항 : email 중복확인 버튼 UI 추가 완료 (/sign-in)

--- app/page.tsx
+++ app/page.tsx
@@ -6,33 +6,37 @@ import { fetchUsers, increment } from "@/slices/userSlice";
 import styles from "./page.module.css";
 import { AppDispatch, RootState } from "@/store/store";
 import { LoginButton } from "@/components/loginButton";
+import axios from "axios";
+import { use } from "react";
 
 interface LayoutProps {
-	Component: any;
-	pageProps: any;
-	store: any;
+  Component: any;
+  pageProps: any;
+  store: any;
 }
 
 export default function Home({ Component, pageProps, store }: LayoutProps) {
   const userRef = useRef(false);
   const { entities, value } = useSelector((state: RootState) => state.user);
   const dispatch = useDispatch<AppDispatch>();
+  // const userData = use(getData());
 
   console.log(entities);
 
   useEffect(() => {
     if (userRef.current === false) {
       dispatch(fetchUsers());
+      console.log(getData2());
     }
     return () => {
       userRef.current = true;
     };
   }, []);
 
-	return (
-		<div className={styles.mainSection}>
-			<h1>처음 화면</h1>
-			<h3>처음 화면의 서브텍스트</h3>
+  return (
+    <div className={styles.mainSection}>
+      <h1>처음 화면</h1>
+      <h3>처음 화면의 서브텍스트</h3>
       <button
         onClick={() => {
           dispatch(increment());
@@ -40,8 +44,59 @@ export default function Home({ Component, pageProps, store }: LayoutProps) {
       >
         증가버튼
       </button>
+      <button
+        onClick={() => {
+          postData();
+        }}
+      >
+        POST 요청
+      </button>
       {value}
-			<LoginButton />
-		</div>
-	);
+      <LoginButton />
+    </div>
+  );
+}
+
+interface userData {
+  id: number;
+  name: string;
+  nickname: string;
+  phonenumber: string;
+  agreement: boolean;
+}
+
+interface Props {
+  userGetData: userData[];
+}
+
+export async function getData() {
+  const response = await axios.get<userData>(
+    "http://localhost:3000/api/getData"
+  );
+  const data = response.data;
+  return data;
+}
+
+const getData2 = () => {
+  axios
+    .get<userData>(`http://localhost:3000/api/getData`)
+    .then((response) => console.log(response.data))
+    .catch((error) => console.log(error));
+};
+
+const postingData = {
+  id: 5,
+  name: "정준하",
+  nickname: "도토아빠",
+  phonenumber: "01022990003",
+  agreement: true,
+};
+
+export async function postData() {
+  const response = await axios.post<userData>(
+    "http://localhost:3000/api/getData",
+    { ...postingData }
+  );
+  const data = response.data;
+  console.log(data);
 }
