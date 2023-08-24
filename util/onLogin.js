df065cd9825ba36658a64b585fe3c3741af4c278
Dropper-in <dropperin59@gmail.com>
2023-08-25 08:31:36 +0900

    #43 feat: onLogin 시 user reducer 실행 및 silent refresh 로직 수정
    로그인 후 새로고침 시 silent refresh가 실행되지 않는 문제 발생
    로그인 시에는 parameter로 dispatch 받아 유저 정보만 저장
    로그인 후 대시보드 내비게이터에서 reissueToken을 실행하도록 silent refresh 로직 수정

--- util/onLogin.js
+++ util/onLogin.js
@@ -1,33 +1,42 @@
 import axios from "axios";
-import { useRouter } from "next/router";
-import { onLogout } from "./onLogout";
+import { login, getToken } from "@/slices/userSlice";
 
-export function onLogin(requestBody) {
+export function onLogin(requestBody, dispatch) {
   axios
     .post("/api/auth/login", requestBody)
     .then((res) => {
       console.log(res);
-      onLoginSuccess(res);
+      dispatch(login(res.data)); // 로그인 시 userSlice의 login 리듀서로 res.data에서 이메일, 이름, 닉네임을 넘겨줌
+      dispatch(getToken(res.headers.authorization)); // accessToken은 이후 silentRefresh를 위해 리듀서를 분리함
+      // 로그인 후 대시보드에서 새로고침 시 onLogin 함수는 이미 실행 완료된 상황이므로 onLoginSuccess 함수 또한 실행되지 않음
+      // onLoginSuccess 삭제 -> 내비게이터에서 reissueToken으로 silent Refresh 수행하도록 변경
     })
     .catch((err) => {
       console.log(err);
     });
-
-  const onLoginSuccess = (res) => {
-    axios.defaults.headers.common["authorization"] = res.headers.authorization;
-    setTimeout(onSilentRefresh, 10000);
-  };
-  const onSilentRefresh = async () => {
-    if (axios.defaults.headers.common["authorization"]) {
-      await axios
-        .get("/api/auth/refresh")
-        .then((res) => {
-          onLoginSuccess(res);
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    }
-  };
 }
+
+const onSilentRefresh = async () => {
+  await axios
+    .get("/api/auth/refresh")
+    .then((res) => {
+      axios.defaults.headers.common["authorization"] =
+        res.headers.authorization;
+      setTimeout(onSilentRefresh, 10000);
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const reissueToken = (accessToken) => {
+  // 로그인 후 새로고침하면 silentRefresh가 일어나지 않음,
+  // -> reissueToken을 네비게이터에서 실행하여 토큰 재발급 실행
+  if (accessToken !== null && accessToken.slice(0, 6) === "Bearer") {
+    axios.defaults.headers.common["Authorization"] = accessToken;
+    onSilentRefresh();
+  } else {
+    console.log("Access Token not defined");
+  }
+};
