069718b7cdeecc13d0ba6cb326086004610c992e
haesol822 <dordork@naver.com>
2023-08-30 16:40:49 +0900

    #75 feat: signup 아이디 중복 체크

--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -208,11 +208,13 @@ export default function SignUp() {
             className={styles.emailCheck} 
             onClick={()=>{
               axios
-              .post('api/user/idcheck', {email:email})
+              .post('api/users/idcheck', {email:email})
               .then(()=>{alert("사용할 수 있는 이메일입니다.")})
-              .catch(()=>{alert("사용할 수 없는 이메일입니다.");
-              setEmail("");
-              })}}>중복확인</button>
+              .catch(
+                ()=>{alert("사용할 수 없는 이메일입니다.");
+                setEmail("");
+              })
+            }}>중복확인</button>
           {email.length > 0 && <span className={`message ${isEmail ? 'success' : 'error'}`}>{emailMessage}</span>}
         </span>
         <h5>이름</h5>
